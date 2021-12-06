2dd0da7e7eb48a411cdd219c4ee9bdc006a76e19
Sricharan1306 <88420805+Sricharan1306@users.noreply.github.com>
2021-12-06 13:27:17 +0530

    Add files via upload

---
+++ download.js
@@ -0,0 +1,24 @@
+document.getElementById("tech").onclick=function(){
+    image= document.getElementById("pic").src;
+    link= document.getElementById("link");
+    link.href= image;
+    link.click();
+}
+document.getElementById("tech2").onclick=function(){
+    image2= document.getElementById("pic2").src;
+    link= document.getElementById("link2");
+    link.href= image2;
+    link.click();
+}
+document.getElementById("tech3").onclick=function(){
+    image3= document.getElementById("pic3").src;
+    link= document.getElementById("link3");
+    link.href= image3;
+    link.click();
+}
+document.getElementById("tech4").onclick=function(){
+    image3= document.getElementById("pic4").src;
+    link= document.getElementById("link4");
+    link.href= image3;
+    link.click();
+}
